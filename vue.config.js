ce4fbccfb044c0eb2cba9b02ed2c819ad5dcb690
jiwei-hemeng <jiwei7065@sina.com>
2021-05-25 11:58:31 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -21,4 +21,10 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "你想设置的title名字";
+      return args;
+    });
+  },
 };
