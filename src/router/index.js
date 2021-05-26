260c2388f32c6ec8959e4de2fdcd8d5a31b5a245
jiwei-hemeng <jiwei7065@sina.com>
2021-05-26 09:41:21 +0800

    路由重定向

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 // const Home = { template: "<div>Home</div>" };
 const routes = [
-  { path: "/", component: () => import("@/views") },
-  { path: "/about", component: () => import("@/views/About") },
+  { path: "/", redirect: { name: "home" } },
+  { path: "/home", name: "home", component: () => import("@/views") },
+  { path: "/about", name: "about", component: () => import("@/views/About") },
 ];
 export default createRouter({
   history: createWebHashHistory(),
