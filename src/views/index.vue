0ad3e70930d4c6eaafdf77c3d8ef23a555f25dde
jiwei-hemeng <jiwei7065@sina.com>
2021-05-27 13:04:46 +0800

    vue3.0的生命周期

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,52 @@
   <div>Home</div>
 </template>
 <script>
+import {
+  reactive,
+  toRefs,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onErrorCaptured,
+} from "vue";
 export default {
   name: "Index",
+  setup() {
+    const state = reactive({});
+    onBeforeMount(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 beforeMount");
+    });
+    onMounted(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 mounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 beforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 updated");
+    });
+    onBeforeUnmount(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 beforeDestroy");
+    });
+    onUnmounted(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 destroyed");
+    });
+    onErrorCaptured(() => {
+      console.log("Counter ===> 相当于 vue2.x 中 errorCaptured");
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
+  mounted() {
+    // vue2.x 中 mounted
+    console.log("vue2.x 中 mounted");
+  },
+  onMounted() {
+    console.log("Counter ===> 相当于 vue2.x 中 mounted");
+  },
 };
 </script>
