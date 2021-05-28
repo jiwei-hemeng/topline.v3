be9c39df51dfd7a7182c229a2a5532a36b431e0e
jiwei-hemeng <jiwei7065@sina.com>
2021-05-28 12:35:03 +0800

    vue3.0 Suspense组件

---
+++ src/components/AyncShow/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ result }}</h1>
+</template>
+<script>
+export default {
+  setup() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ result: 100 });
+      }, 2000);
+    });
+  },
+};
+</script>
--- src/views/refDom/index.vue
+++ src/views/refDom/index.vue
@@ -7,12 +7,29 @@
     </li>
   </ul>
   <input type="number" name="userName" id="" ref="dom" />
+  <!-- Suspense是Vue3.0推出的一个内置特殊组件，用来定义具有异步请求数据的组建的显示。如果使用Suspense，要setup函数中需要返回一个promise. -->
+  <!-- Suspense组件内置了两个具名插槽slot,一个是default，用来显示异步组件请求成功的内容；一个是fallback用来显示异步组件请求响应前页面显示的内容 -->
+  <!-- default插槽可以有多个组件，但是需要有一个根节点 -->
+  <Suspense>
+    <template v-slot:default>
+      <div>
+        <AsyncShow />
+      </div>
+    </template>
+    <template v-slot:fallback>
+      <h1>Loading...</h1>
+    </template>
+  </Suspense>
 </template>
 
 <script>
 import { ref, nextTick } from "vue";
+import AsyncShow from "@/components/AyncShow/index.vue";
 export default {
   name: "refDom",
+  components: {
+    AsyncShow,
+  },
   setup() {
     // 单个dom获取
     const dom = ref(null);
