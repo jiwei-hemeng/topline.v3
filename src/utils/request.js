e795b0828fcdb51a1481e859524d2751c56c3c42
jiwei-hemeng <jiwei7065@sina.com>
2021-05-28 16:34:19 +0800

    vue3.0 request的封装

---
+++ src/utils/request.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const baseURL = process.env.VUE_APP_BASE_URL;
+let Request = axios.create({
+  baseURL,
+});
+// 请求拦截器
+Request.interceptors.request.use((config) => {
+  return config;
+});
+// 响应拦截器
+Request.interceptors.response.use((response) => {
+  return response;
+});
+export default Request;
