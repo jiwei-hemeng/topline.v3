58f861fb0eaa63ca4e53f029d455c28168e7c6dc
jiwei-hemeng <jiwei7065@sina.com>
2021-05-28 16:36:15 +0800

    12

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,8 @@
   </div>
 </template>
 <script>
-import axios from "axios";
 export default {
   name: "App",
-  mounted() {
-    console.log("baseurl", process.env.VUE_APP_BASE_URL);
-    axios
-      .get(process.env.VUE_APP_BASE_URL + "index/recommend.json")
-      .then((res) => {
-        console.log(res.data.list);
-      });
-  },
 };
 </script>
 <style lang="scss" scoped>
