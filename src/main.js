900e32cf943165a14587717e3be2462421f23a7c
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2021-08-11 18:30:55 +0800

    vue3.0中的自定义指令

--- src/main.js
+++ src/main.js
@@ -11,4 +11,11 @@ app.use(Vant);
 app.use(Lazyload);
 app.config.globalProperties.$title = "vue3.0 测试";
 app.provide("$message", "哈喽，我是测试");
+app.directive("focus", {
+  // 当被绑定的元素挂载到 DOM 中时……
+  mounted(el) {
+    // 聚焦元素
+    el.focus();
+  },
+});
 app.mount("#app");
