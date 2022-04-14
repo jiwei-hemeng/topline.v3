0d07fd76755e710d56738cf0345dc40c84618d21
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2022-04-14 18:17:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:jiwei-hemeng/topline.v3.git master:gh-pages
+git push -f git@github.com:jiwei-hemeng/topline.v3.git main:gh-pages
 
 cd -
